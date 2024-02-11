52741a343e3878894b62f2f1c93cc84f50835bcd
Kaustubhvekhande <kaustubhvekhande206@gmail.com>
2024-02-11 15:02:46 +0530

    changes in lines.java

--- Other Programs/Encapsulation/Line.java
+++ Other Programs/Encapsulation/Line.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 public class Line
 {
 	private double slope;
--- Other Programs/Encapsulation/Lines.java
+++ Other Programs/Encapsulation/Lines.java
@@ -13,33 +13,32 @@ public class Lines {
         y_int = 0;
     }
 
-    Lines(double x1, double y1, double x2, double y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.slope = getSlope();
-        this.y_int = y1 - slope * x1;
-    }
-
     double getSlope() {
         return (y2 - y1) / (x2 - x1);
     }
 
-    void interceptForm() {
-
+    Lines(double m, double y_int) {
+        this.slope = m;
+        this.y_int = y_int;
         y_int = y1 - slope * x1;
 
         System.out.printf("The Equation of line in slope-intercept formt is: y = %.2fx + %.2f\n", slope, y_int);
     }
 
-    void twoPoint() {
+    Lines(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.x2= x2;
+        this.y1 = y1;
+        this.y2 = y2;
         System.out.printf("The Equation of line in two point form is : (y -%.2f)/%.2f = %.2f (x -%.2f)/(%.2f)\n", y1,
                 y1 - y2,
                 getSlope(), x1, x1 - x2);
     }
 
-    void onePoint() {
+    Lines(double x1, double y1, double m) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.slope = m;
         System.out.printf("The Equation of line in one point form is : (y -%.2f) = %.2f (x -%.2f)\n", y1,
                 getSlope(), x1);
     }
@@ -47,20 +46,7 @@ public class Lines {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter value of x1:");
-        double x1 = sc.nextDouble();
-        System.out.println("Enter value of y1:");
-        double y1 = sc.nextDouble();
-
-        System.out.println("Enter value of x2:");
-        double x2 = sc.nextDouble();
-        System.out.println("Enter value of y2:");
-        double y2 = sc.nextDouble();
-
-        Lines l = new Lines(x1, y1, x2, y2);
-
-        double m = l.getSlope();
-        System.out.printf("The slope of given line is: %.2f\n", m);
+        double x1, x2, y1, y2, m, y_int;
 
         loop: while (true) {
 
@@ -73,13 +59,26 @@ public class Lines {
 
             switch (choice) {
                 case 1:
-                    l.twoPoint();
+
+                    System.out.println("Enter the x1,y1,x2,y2 values for line");
+                    x1 = sc.nextDouble();
+                    y1 = sc.nextDouble();
+                    x2 = sc.nextDouble();
+                    y2 = sc.nextDouble();
+                    Lines l1 = new Lines(x1, y1, x2, y2);
                     break;
                 case 2:
-                    l.onePoint();
+                    System.out.println("Enter the values of x1,y1 and slope for line");
+                    x1 = sc.nextDouble();
+                    y1 = sc.nextDouble();
+                    m = sc.nextDouble();
+                    Lines l2 = new Lines(x1, y1, m);
                     break;
                 case 3:
-                    l.interceptForm();
+                    System.out.println("Enter values of slope and y-intercept for line");
+                    m = sc.nextDouble();
+                    y_int = sc.nextDouble();
+                    Lines l3 = new Lines(m, y_int);
                     break;
                 case 4:
                     System.out.println("Exiting...");
